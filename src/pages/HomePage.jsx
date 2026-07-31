097bb971d22364e2631c41660eee2619098a9e31
Gürhan Aras <gurhanaras89@gmail.com>
2026-07-31 20:36:30 +0300

    chore: complete project setup

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+function HomePage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-600">
+        NextG E-Commerce 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default HomePage;
