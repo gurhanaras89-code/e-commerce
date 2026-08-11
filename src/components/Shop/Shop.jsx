268249ca6dbfcc36a629382faf4179226f0708f9
Gürhan Aras <gurhanaras89@gmail.com>
2026-08-11 21:24:29 +0300

    t02 tamam

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,23 @@
+function Shop() {
+  return (
+    <main>
+      <section className="shop-page">
+        <div className="shop-container">
+
+          <div className="shop-heading">
+            <h1>Shop</h1>
+
+            <div className="breadcrumb">
+              <span>Home</span>
+              <span>›</span>
+              <span>Shop</span>
+            </div>
+          </div>
+
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default Shop;
